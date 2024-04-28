1a90226c7d7b0800f5f326963da471bc3e7738b4
Đinh Viết Danh <dinhvietdanh1805@gmail.com>
2024-04-28 17:16:30 +0700

    Update

--- js/particle.js
+++ js/particle.js
@@ -1,114 +1,98 @@
 /* ---- particles.js config ---- */
 
-particlesJS('particles-js', {
-  "particles": {
-    "number": {
-      "value": 199,
-      "density": {
-        "enable": true,
-        "value_area": 946.9771699587272
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 140,
+      density: {
+        enable: true,
+        value_area: 800,
       },
-      "polygon": {
-        "nb_sides": 5
+    },
+    color: {
+      value: ["#F9E8D9"],
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#fff",
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
+        width: 100,
+        height: 100,
+      },
     },
-    "opacity": {
-      "value": 0.5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
     },
-    "size": {
-      "value": 10,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 10,
+        sync: false,
+      },
     },
-    "line_linked": {
-      "enable": false,
-      "distance": 500,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 2
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1,
     },
-    "move": {
-      "enable": true,
-      "speed":3 ,
-      "direction": "bottom",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "bubble"
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
       },
-      "onclick": {
-        "enable": true,
-        "mode": "repulse"
+      onclick: {
+        enable: true,
+        mode: "push",
       },
-      "resize": true
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 0.5
-        }
+    modes: {
+      repulse: {
+        distance: 70,
+        duration: 0.4,
       },
-      "bubble": {
-        "distance": 400,
-        "size": 4,
-        "duration": 0.3,
-        "opacity": 1,
-        "speed": 3
+      push: {
+        particles_nb: 4,
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
+    },
   },
-  "retina_detect": true
+  retina_detect: true,
 });
 const allElements = document.querySelectorAll(".animated-text");
 
